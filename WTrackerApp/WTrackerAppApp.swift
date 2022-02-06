f2e6c872b01f236c50eb68ee631de2ee859a6b57
huangbaron2 <huangbaron2@gmail.com>
2022-02-06 16:10:20 -0500

    Initial Commit

---
+++ WTrackerApp/WTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTrackerAppApp.swift
+//  WTrackerApp
+//
+//  Created by Baron on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct WTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
